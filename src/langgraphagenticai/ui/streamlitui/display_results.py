02bac6bd7056bcf05d823e31a1e4f97dd99df36e
sandeepkr23 <sandeep2tej@gmail.com>
2025-03-26 22:01:24 +0530

    YML commit

--- src/langgraphagenticai/ui/streamlitui/display_results.py
+++ src/langgraphagenticai/ui/streamlitui/display_results.py
@@ -2,10 +2,9 @@ import streamlit as st
 from langchain_core.messages import HumanMessage,AIMessage,ToolMessage
 import json
 
-
 class DisplayResultStreamlit:
-    def __init__(self,usecase,graph,user_message):
-        self.usecase= usecase
+    def __init__(self, usecase, graph, user_message):
+        self.usecase = usecase
         self.graph = graph
         self.user_message = user_message
 
@@ -13,15 +12,16 @@ class DisplayResultStreamlit:
         usecase= self.usecase
         graph = self.graph
         user_message = self.user_message
-        if usecase =="Basic Chatbot":
-                for event in graph.stream({'messages':("user",user_message)}):
-                    print(event.values())
-                    for value in event.values():
-                        print(value['messages'])
-                        with st.chat_message("user"):
-                            st.write(user_message)
-                        with st.chat_message("assistant"):
-                            st.write(value["messages"].content)
+
+        if usecase == "Basic Chatbot":
+            for event in graph.stream({'messages':("user",user_message)}):
+                print(event.values())
+                for value in event.values():
+                    print(value['messages'])
+                    with st.chat_message("user"):
+                        st.write(user_message)
+                    with st.chat_message("assistant"):
+                        st.write(value["messages"].content)
 
         elif usecase=="Chatbot with Tool":
              # Prepare state and invoke the graph
